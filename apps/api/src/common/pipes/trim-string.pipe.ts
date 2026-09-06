e56dd1ce00567d4dd5d7bd78dbfa6d0e72f70670
mrityunjay45108 <kumarmrityunjay5210@gmail.com>
2026-09-06 12:20:58 +0530

    chore(iteration-36): improve project

---
+++ apps/api/src/common/pipes/trim-string.pipe.ts
@@ -0,0 +1,18 @@
+import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+
+@Injectable()
+export class TrimStringPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (typeof value === 'string') {
+      return value.trim();
+    }
+    if (typeof value === 'object' && value !== null) {
+      for (const key of Object.keys(value)) {
+        if (typeof value[key] === 'string') {
+          value[key] = value[key].trim();
+        }
+      }
+    }
+    return value;
+  }
+}
