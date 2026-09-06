58eb2590c73831bbe068e882ef38a879c1f99629
mrityunjay45108 <kumarmrityunjay5210@gmail.com>
2026-09-06 12:20:56 +0530

    chore(iteration-35): improve project

---
+++ apps/api/src/common/constants/api-messages.constants.ts
@@ -0,0 +1,8 @@
+export const API_MESSAGES = {
+  COUPON_ALREADY_REDEEMED: 'You have already redeemed this coupon code in a previous order.',
+  COUPON_EXPIRED: 'This coupon code is expired or inactive.',
+  COUPON_MIN_ORDER_UNMET: 'Order total does not meet the minimum requirement for this coupon.',
+  PRODUCT_NOT_FOUND: 'The requested product could not be found.',
+  CART_EMPTY: 'Your cart is currently empty.',
+  ORDER_PLACED_SUCCESS: 'Your order has been placed successfully!',
+} as const;
